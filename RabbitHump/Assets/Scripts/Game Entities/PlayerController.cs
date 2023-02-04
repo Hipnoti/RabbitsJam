5fad702e63c10ab37ce4bf15534f0960244c92b8
Lior Hadashian <lior@gavra-games.com>
2023-02-04 20:33:47 +0200

    W.IP

--- RabbitHump/Assets/Scripts/Game Entities/PlayerController.cs
+++ RabbitHump/Assets/Scripts/Game Entities/PlayerController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public enum Players {Player1, Player2}
